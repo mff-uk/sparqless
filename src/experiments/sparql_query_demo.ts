4a43f86729bed44856f63358ae99de808da94d24
Daniel Crha <daniel.crha@gmail.com>
2021-11-07 21:01:03 +0100

    feat(test): add test code for SPARQL query

--- src/experiments/sparql_query_demo.ts
+++ src/experiments/sparql_query_demo.ts
@@ -1,13 +1,34 @@
-// Just to test that TS debugging is set up correctly...
-interface ITest {
+import { Quad, Stream } from "rdf-js";
+import StreamClient from "sparql-http-client"
 
-}
+class TestClass {
+    async runTest(): Promise<number> {
+        const client = new StreamClient({ endpointUrl: 'https://query.wikidata.org/sparql' });
+        const startTime = performance.now();
 
-class TestClass implements ITest {
-    runTest(): void {
-        console.log("hello world!")
-        console.log("2!")
+        const stream = await client.query.construct('DESCRIBE <http://www.wikidata.org/entity/Q54872>');
+        const endTime = await this.measureQueryEnd(stream);
+
+        return endTime - startTime;
+    }
+
+    measureQueryEnd(stream: Stream<Quad>): Promise<number> {
+        return new Promise((resolve, reject) => {
+            let quadCount = 0;
+            stream.on('data', _quad => {
+                quadCount += 1;
+            });
+            stream.on('end', () => {
+                console.log(`Got ${quadCount} quads.`)
+                resolve(performance.now());
+            });
+            stream.on('error', error => {
+                reject(error);
+            });
+        });
     }
 }
 
-(new TestClass()).runTest()
+(new TestClass()).runTest().then(value => {
+    console.log(`Test done! Test took ${value}ms.`);
+});
